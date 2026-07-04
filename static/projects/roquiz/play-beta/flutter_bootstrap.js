52b900f27bfb0c8270de32ae88116e1eb4f4cffe
mikyll <righi.michy@gmail.com>
2026-07-04 04:25:16 +0200

    fix(projects): update roquiz beta

--- static/projects/roquiz/play-beta/flutter_bootstrap.js
+++ static/projects/roquiz/play-beta/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2241603899" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3735132081" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
